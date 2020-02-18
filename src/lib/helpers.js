c34d2aa34b9f6cb3395025a0a2eef5e2b529f3c0
Jorge <jorgediaz83@gmail.com>
2020-02-17 22:42:04 -0600

    autenticacion

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,12 +3,8 @@ const bcrypt = require('bcryptjs');
 const helpers = {};
 
 helpers.encryptPassword = async(password) => {
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-    } catch (e) {
-        console.log(e);
-    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
     return hash;
 };
 
